571e7afc90bd604661fb4e031dd859e56d51aaf1
Maxim Tsurkanov <max86ts@gmail.com>
2023-06-26 19:39:11 +0300

    1. Spring Liquibase [#302330] Рефакторинг. К модулю auth подключена и настроена библиотека liquibase: -./auth/pom.xml: добавлены зависимости commons-dbcp2(v.2.9.0), liquibase-core(v.4.22.0); -./auth application.properties: добавлено свойство spring.liquibase.change-log. - создан файл liquibase-changeLog.xml; - рефакторинг AuthApplication.java: созданы бины: DataSource(), liquibase(). - дирректория /db со скриптами перемещена в classpath

--- auth/src/main/java/ru/job4j/auth/AuthApplication.java
+++ auth/src/main/java/ru/job4j/auth/AuthApplication.java
@@ -1,19 +1,53 @@
 package ru.job4j.auth;
 
+import liquibase.integration.spring.SpringLiquibase;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication
-public class AuthApplication {
+import javax.sql.DataSource;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthApplication.class, args);
+@SpringBootApplication
+public class AuthApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AuthApplication.class);
+	}
+
+	@Bean
+	public DataSource ds(@Value("${spring.datasource.driver-class-name}") String driver,
+						 @Value("${spring.datasource.url}") String url,
+						 @Value("${spring.datasource.username}") String username,
+						 @Value("${spring.datasource.password}") String password) {
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName(driver);
+		dataSource.setUrl(url);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	@Bean
+	public SpringLiquibase liquibase(DataSource ds,
+									 @Value("${spring.liquibase.change-log}") String changeLogFile) {
+		SpringLiquibase liquibase = new SpringLiquibase();
+		liquibase.setDataSource(ds);
+		liquibase.setChangeLog(changeLogFile);
+		return liquibase;
 	}
 
 	@Bean
 	public BCryptPasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
 	}
 }
